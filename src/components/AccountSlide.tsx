624db59634a9498f80b8d4e2d3f214a7c9395925
Eduard Baron <ebaron@modyo.com>
2023-08-23 13:08:36 -0500

    chore: add slides components

---
+++ src/components/AccountSlide.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/destructuring-assignment */
+import {
+  MCardAccount,
+} from '@dynamic-framework/ui-react';
+import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ACCOUNT_PATHS, SITE_URL } from '../config/widgetConfig';
+import type { Account } from '../services/interface';
+import { AccountTypeConfig } from '../services/config';
+import useAccountValue from '../hooks/useAccountValue';
+
+type Props = {
+  account: Account;
+};
+
+export default function AccountSlide({ account }: Props) {
+  const { t } = useTranslation();
+
+  const accountPath = useCallback(() => {
+    window.location.href = `${SITE_URL}/${ACCOUNT_PATHS[account.type]}?product_id=${account.id}`;
+  }, [account.id, account.type]);
+
+  const { label, value } = useAccountValue(account);
+
+  return (
+
+    <MCardAccount
+      className="flex-grow-1"
+      icon={AccountTypeConfig[account.type].icon}
+      theme={AccountTypeConfig[account.type].theme}
+      name={account.alias || account.name}
+      number={account.accountNumber}
+      balance={value}
+      balanceText={label}
+      onClickText={t('details')}
+      onClick={accountPath}
+    />
+  );
+}
