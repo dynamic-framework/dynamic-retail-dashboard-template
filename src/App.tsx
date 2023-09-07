4277ec124c5c5ce35fadfd46397dacb0bee5554b
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-06 22:19:29 -0500

    style: remove container

--- src/App.tsx
+++ src/App.tsx
@@ -37,70 +37,68 @@ export default function App() {
   );
 
   return (
-    <div>
-      <div className="row">
-        <div className="col-12 col-lg-8">
-          <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center mb-3">
-            <h1 className="fs-4 fw-bold w-100">{t('my-accounts')}</h1>
-            {accounts.length > 0 && (
-              <div className="d-flex flex-grow-1 w-100 justify-content-end">
-                <DButton
-                  iconStart={data.icon}
-                  text={data.label}
-                  variant="link"
-                  theme="secondary"
-                  onEventClick={callback}
-                />
-                {/* For Mobile */}
-                <DButton
-                  iconStart="view-list"
-                  variant="link"
-                  theme="secondary"
-                  onEventClick={() => handlerView('list')}
-                  className="d-block d-lg-none"
-                />
-                <DButton
-                  iconStart="grid"
-                  variant="link"
-                  theme="secondary"
-                  onEventClick={() => handlerView('slides')}
-                  className="d-block d-lg-none"
-                />
-                {/* For Desktop */}
-                <DButton
-                  iconStart="view-list"
-                  text={t('list')}
-                  variant="link"
-                  theme="secondary"
-                  onEventClick={() => handlerView('list')}
-                  className="d-none d-lg-block"
-                />
-                <DButton
-                  iconStart="grid"
-                  text={t('slides')}
-                  variant="link"
-                  theme="secondary"
-                  onEventClick={() => handlerView('slides')}
-                  className="d-none d-lg-block"
-                />
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="col-12 col-lg-8">
-          <CurrentView />
-          <div className="row">
-            <h5 className="fw-bold py-3">
-              {t('transactions')}
-            </h5>
-            <div className="d-block col">
-              <LatestActivitiesList />
+    <div className="row">
+      <div className="col-12 col-lg-8">
+        <div className="d-flex flex-column flex-lg-row justify-content-between align-items-center mb-3">
+          <h1 className="fs-4 fw-bold w-100">{t('my-accounts')}</h1>
+          {accounts.length > 0 && (
+            <div className="d-flex flex-grow-1 w-100 justify-content-end">
+              <DButton
+                iconStart={data.icon}
+                text={data.label}
+                variant="link"
+                theme="secondary"
+                onEventClick={callback}
+              />
+              {/* For Mobile */}
+              <DButton
+                iconStart="view-list"
+                variant="link"
+                theme="secondary"
+                onEventClick={() => handlerView('list')}
+                className="d-block d-lg-none"
+              />
+              <DButton
+                iconStart="grid"
+                variant="link"
+                theme="secondary"
+                onEventClick={() => handlerView('slides')}
+                className="d-block d-lg-none"
+              />
+              {/* For Desktop */}
+              <DButton
+                iconStart="view-list"
+                text={t('list')}
+                variant="link"
+                theme="secondary"
+                onEventClick={() => handlerView('list')}
+                className="d-none d-lg-block"
+              />
+              <DButton
+                iconStart="grid"
+                text={t('slides')}
+                variant="link"
+                theme="secondary"
+                onEventClick={() => handlerView('slides')}
+                className="d-none d-lg-block"
+              />
             </div>
+          )}
+        </div>
+      </div>
+      <div className="col-12 col-lg-8">
+        <CurrentView />
+        <div className="row">
+          <h5 className="fw-bold py-3">
+            {t('transactions')}
+          </h5>
+          <div className="d-block col">
+            <LatestActivitiesList />
           </div>
         </div>
-        <div className="d-none d-lg-block col-lg-4">
-          <QuickTransfer />
-        </div>
+      </div>
+      <div className="d-none d-lg-block col-lg-4">
+        <QuickTransfer />
       </div>
     </div>
   );
