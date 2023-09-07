92a06fd2bdc5f76e1851dab9fa46b729c0cedf48
Johann Felipe Rojas <johannrip@gmail.com>
2023-09-06 19:31:17 -0500

    refactor: use DListMovement

--- src/components/LatestActivitiesList.tsx
+++ src/components/LatestActivitiesList.tsx
@@ -1,40 +1,31 @@
-import classNames from 'classnames';
-import { useFormatCurrency } from '@dynamic-framework/ui-react';
-import { DateTime } from 'luxon';
+import { DList, DListItemMovement } from '@dynamic-framework/ui-react';
 import ActivityListLoader from './loaders/ActivityListLoader';
 import useTransactions from '../services/hooks/useLatestActivities';
 
 export default function LatestActivitiesList() {
   const { data, loading } = useTransactions();
-  const { format } = useFormatCurrency();
 
   if (loading) {
     return <ActivityListLoader />;
   }
 
   return (
-    <div className="bg-white rounded px-4">
-      {data.map((transaction) => (
-        <div
-          key={transaction.id}
-          className="py-3 d-flex align-items-center border-bottom"
-        >
-          <div className="d-flex flex-column flex-grow-1">
-            <p className="h6">{transaction.description}</p>
-            <small className="text-light-emphasis">
-              {DateTime.fromISO(transaction.effectiveDate).toFormat('dd/MM/yyyy')}
-            </small>
-          </div>
-          <p className={classNames({
-            h6: true,
-            'text-danger': transaction.amount < 0,
-            'text-success': transaction.amount >= 0,
-          })}
-          >
-            {`${transaction.amount < 0 ? '-' : '+'}${format(transaction.amount)}`}
-          </p>
-        </div>
-      ))}
+    <div className="bg-white rounded px-3">
+      <DList isFlush>
+        {data.map(({
+          id,
+          amount,
+          description,
+          effectiveDate,
+        }) => (
+          <DListItemMovement
+            key={id}
+            amount={amount}
+            description={description}
+            date={effectiveDate}
+          />
+        ))}
+      </DList>
     </div>
   );
 }
