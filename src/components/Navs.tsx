1cd5f8c5c9e42b8388e66ea1a253285eb13e57b5
Eduard Baron <ebaron@modyo.com>
2024-10-18 12:36:48 -0500

    chore: add navs

---
+++ src/components/Navs.tsx
@@ -0,0 +1,64 @@
+import { DButton, DIcon } from '@dynamic-framework/ui-react';
+import classNames from 'classnames';
+import { useTranslation } from 'react-i18next';
+
+import { View } from '../config/widgetConfig';
+import useToggleBalances from '../hooks/useToggleBalances';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getAccounts, getCurrentView } from '../store/selectors';
+import { setCurrentView } from '../store/slice';
+
+const OPTIONS: Array<{ icon: string, view: View }> = [
+  {
+    icon: 'list-check',
+    view: 'list',
+  },
+  {
+    icon: 'layout-three-columns',
+    view: 'slides',
+  },
+];
+
+export default function Navs() {
+  const accounts = useAppSelector(getAccounts);
+  const { data, callback } = useToggleBalances();
+  const currentView = useAppSelector(getCurrentView);
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation();
+
+  if (accounts.length < 1) {
+    return null;
+  }
+
+  return (
+    <ul className="nav nav-pills gap-1 p-0 justify-content-end">
+      <DButton
+        iconStart={data.icon}
+        text={data.label}
+        variant="link"
+        className="px-4 py-2"
+        onClick={callback}
+      />
+      {OPTIONS.map(({ icon, view }) => (
+        <li className="nav-item" key={view}>
+          <button
+            className={classNames(
+              'nav-link d-inline-flex align-items-center gap-1',
+              // Remove after release - start
+              { 'rounded-1': true },
+              { 'bg-primary': view === currentView },
+              // Remove after release - end
+              { 'bg-white': view !== currentView },
+              { active: view === currentView },
+            )}
+            type="button"
+            onClick={() => dispatch(setCurrentView(view))}
+          >
+            <DIcon icon={icon} size="var(--bs-ref-spacer-4)" />
+            {t(view)}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
