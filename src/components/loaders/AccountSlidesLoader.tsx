a5e382fbe5215fa198f8f5ba55bc6e286c888aa9
Eduard Baron <ebaron@modyo.com>
2024-10-21 19:08:28 -0500

    chore: remove bg-secondary-200 in loaders

--- src/components/loaders/AccountSlidesLoader.tsx
+++ src/components/loaders/AccountSlidesLoader.tsx
@@ -2,13 +2,13 @@ export default function AccountSlidesLoader() {
   return (
     <div className="d-flex placeholder-glow gap-4 mt-4">
       <span
-        className="placeholder bg-secondary-200 col rounded-2"
+        className="placeholder col rounded-2"
         style={{
           height: '13rem',
         }}
       />
       <span
-        className="d-none d-lg-block placeholder bg-secondary-200 col rounded-2"
+        className="d-none d-lg-block placeholder col rounded-2"
         style={{
           height: '13rem',
         }}
--- src/components/loaders/CategoryListLoader.tsx
+++ src/components/loaders/CategoryListLoader.tsx
@@ -3,7 +3,7 @@ export default function CategoryListLoader() {
     <div className="d-flex flex-column gap-4 placeholder-glow">
       <div className="d-flex bg-white p-4 rounded-1">
         <span
-          className="placeholder bg-secondary-200 rounded-2 col"
+          className="placeholder rounded-2 col"
           style={{
             height: '10rem',
           }}
@@ -11,7 +11,7 @@ export default function CategoryListLoader() {
       </div>
       <div className="d-flex bg-white p-4 rounded-1">
         <span
-          className="placeholder bg-secondary-200 rounded-2 col"
+          className="placeholder rounded-2 col"
           style={{
             height: '10rem',
           }}
@@ -19,7 +19,7 @@ export default function CategoryListLoader() {
       </div>
       <div className="d-flex bg-white p-4 rounded-1">
         <span
-          className="placeholder bg-secondary-200 rounded-2 col"
+          className="placeholder rounded-2 col"
           style={{
             height: '10rem',
           }}
